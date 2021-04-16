862aea9ca67a7cf75723cc70c0f404128a306b59
neozumm <neozumm@rambler.ru>
2021-04-16 18:47:06 +0300

    error objects in jrpc

--- main.go
+++ main.go
@@ -61,13 +61,13 @@ func requestApi(hub *Hub, w http.ResponseWriter, r *http.Request, rdb *redis.Cli
 	msg, err := decodeJSONMessage(encodedMessage)
 	if err != nil {
 		log.Println("failed to decode message: ", msg)
-		w.Write([]byte(fmt.Sprintf(`{"jsonrpc": "2.0", "result":"failed, message not decoded", id:"%d"}`, msg.Id)))
+		w.Write([]byte(fmt.Sprintf(`{"jsonrpc": "2.0", "result":"failed", "error":{"code":1, "message":"message not decoded"}, id:"%d"}`, msg.Id)))
 		return
 	}
 	errMessage := checkDecodedMessage(msg)
 	if errMessage != nil {
 		log.Println("decoded message does not contain needed info", errMessage)
-		w.Write([]byte(fmt.Sprintf(`{"jsonrpc": "2.0", "result":"failed, message contained incorrect data", id:"%d"}`, msg.Id)))
+		w.Write([]byte(fmt.Sprintf(`{"jsonrpc": "2.0", "result":"failed", "error":{"code":2, "message":"message contained incorrect data"}, id:"%d"}`, msg.Id)))
 		return
 	}
 	rdb.XAdd(ctx, &redis.XAddArgs{
