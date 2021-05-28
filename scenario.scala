aff9d3e1d3e07e4bb4a340bf1234b4ce21e3bcd9
neozumm <neozumm@rambler.ru>
2021-05-28 20:53:41 +0300

    scala file fix

--- scenario.scala
+++ scenario.scala
@@ -7,21 +7,23 @@ import io.gatling.http.Predef._
 import io.gatling.http.protocol.HttpProtocolBuilder
 
 class Websocket extends Simulation {
-  val httpProtocol: HttpProtocolBuilder = http.baseUrl("192.168.0.100").wsBaseUrl("ws://192.168.0.100/ws")
+  val httpProtocol: HttpProtocolBuilder = http.baseUrl("http://192.168.0.100:8080").wsBaseUrl("ws://localhost:8080")
 
 //root is HTTP protocol and webSocket url is being appended to it
 
   val scene = scenario("testWebSocket")
     .exec(http("firstRequest")
     .get("/"))
-    .exec(addCookie(Cookie("UID", "111")))
-    .exec(ws("openSocket").connect("/"))
-    .pause(20)
+    .exec(addCookie(Cookie("UID", "111").withDomain("localhost")))
+    .exec(addCookie(Cookie("lastID", "0").withDomain("localhost")))
+    .pause(1)
+    .exec(ws("openSocket").connect("/ws"))
+    .pause(30.seconds)
     .exec(ws("closeConnection").close)
 //terminating the current websocket connection
 
   setUp(scene.inject(
-    atOnceUsers(20)
-    rampUsers(100).during(20.seconds)
+    atOnceUsers(20),
+    rampUsers(200).during(20.seconds)
     ).protocols(httpProtocol))
 }
